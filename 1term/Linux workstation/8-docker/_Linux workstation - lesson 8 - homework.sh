d15818aac22380bef4e45866828e19789f828b86
alex-coch <a@deonop.com>
2021-10-04 11:53:08 +0400

    8-docker

---
+++ 1term/Linux workstation/8-docker/_Linux workstation - lesson 8 - homework.sh
@@ -0,0 +1,37 @@
+####################################################### Разбор ДЗ-8
+
+# 2. Подключить репозиторий Docker.
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+sudo apt update
+
+sudo apt install docker -y
+
+sudo apt install docker.io -y
+
+
+# 3. Запустить контейнер с Ubuntu.
+sudo docker run -it ubuntu
+
+
+# 4. * Используя Dockerfile, собрать связку nginx + PHP-FPM в одном контейнере.
+
+cat > Dockerfile
+FROM ubuntu:latest
+MAINTAINER GB_student
+RUN apt-get update
+RUN apt-get install nginx -y
+RUN apt-get install php-fpm -y
+RUN mkdir /run/php-fpm
+VOLUME "/var/www/html"
+EXPOSE 80
+CMD /usr/sbin/nginx -g "daemon off;" ; php-fpm -D
+
+sudo docker build -t php_fpm .
+
+sudo docker images
+
