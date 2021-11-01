228062c35711fc97cd253e4b3f7324764d1ec422
alex-coch <a@deonop.com>
2021-11-01 09:40:34 +0400

    hm5.sql.2

--- 1term/Relational databases.MySQL/5-aggregation, filtering, sorting, limiting/hm5.sql
+++ 1term/Relational databases.MySQL/5-aggregation, filtering, sorting, limiting/hm5.sql
@@ -1,3 +1,4 @@
+/*  Уќператоры, фильтраци€, сортировка и ограничениеФ */
 /*1*/
 ALTER TABLE users ADD updated_at DATETIME ON UPDATE CURRENT_TIMESTAMP;
 UPDATE users SET created_at=NOW();
@@ -7,4 +8,20 @@ UPDATE users SET created_at=DATE_FORMAT(STR_TO_DATE(created_at, '%e.%c.%Y %H:%i'
 
 /*3*/
 SELECT * FROM storehouses_products ORDER BY CASE WHEN value = 0 THEN 2147483647 ELSE value END;
+
+/*4*/
+SELECT * FROM profiles WHERE LOWER(MONTHNAME(birthday)) in ('may', 'august') 
+
+/*5*/
+SELECT * FROM catalogs WHERE id IN (5, 1, 2) ORDER BY FIELD(ID,5,1,2) 
+
+/* Ујгрегаци€ данныхФ */
+/*1*/
+SELECT AVG(TIMESTAMPDIFF(YEAR, birthday, NOW())) FROM `profiles` JOIN users ON users.id = profiles.user_id 
+
+/*2*/
+SELECT DAYOFWEEK(birthday) AS day, count(*) FROM `profiles` GROUP BY day
+
+/*3*/
+select ROUND(exp(sum(ln(id))), 0) from users
 
