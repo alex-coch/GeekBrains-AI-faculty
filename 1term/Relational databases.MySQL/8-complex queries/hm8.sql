bb7d38e6378ce46a441b935e8660d19c0ae332ae
alex-coch <a@deonop.com>
2021-11-02 09:24:37 +0400

    hm8.sql

--- 1term/Relational databases.MySQL/8-complex queries/hm8.sql
+++ 1term/Relational databases.MySQL/8-complex queries/hm8.sql
@@ -1,2 +1,8 @@
 /*1*/
-SELECT (SELECT CONCAT(firstname, ' ', lastname) FROM users WHERE id = from_user_id), from_user_id, count(*) AS amount FROM `messages` JOIN users ON users.id=messages.from_user_id OR users.id=messages.to_user_id WHERE to_user_id=1 GROUP BY from_user_id ORDER BY amount DESC LIMIT 1 
+SELECT (SELECT CONCAT(firstname, ' ', lastname) FROM users WHERE id = from_user_id), from_user_id, count(*) AS amount FROM `messages` JOIN users ON users.id=messages.from_user_id OR users.id=messages.to_user_id WHERE to_user_id=1 GROUP BY from_user_id ORDER BY amount DESC LIMIT 1;
+
+/*2*/
+SELECT count(likes.id) FROM profiles JOIN media ON profiles.user_id=media.user_id JOIN likes ON likes.media_id=media.id WHERE TIMESTAMPDIFF(YEAR, birthday, NOW()) < 10;
+
+/*3*/
+SELECT CASE (gender) WHEN 'f' THEN 'female' WHEN 'm' THEN 'male' WHEN 'x' THEN 'not defined' END AS gen, count(gender) AS amount FROM profiles JOIN media ON profiles.user_id=media.user_id JOIN likes ON likes.media_id=media.id GROUP BY gen ORDER BY amount DESC 
