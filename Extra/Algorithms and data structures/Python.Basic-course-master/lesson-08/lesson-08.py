be9eedf26d962e2ae9859ea0b949c254435c1073
alex-coch <a@deonop.com>
2022-11-01 10:39:15 +0400

    interim commit

--- Extra/Algorithms and data structures/Python.Basic-course-master/lesson-08/lesson-08.py
+++ Extra/Algorithms and data structures/Python.Basic-course-master/lesson-08/lesson-08.py
@@ -10,7 +10,9 @@ sum_substring = set()
 
 for i in range(len(string)):
     for j in range(len(string), i, -1):
+        print(string[i:j], string[i:j].encode('utf-8').__hash__(), hashlib.sha1(string[i:j].encode('utf-8')).hexdigest())
         hash_str = hashlib.sha1(string[i:j].encode('utf-8')).hexdigest()
         sum_substring.add(hash_str)
 
 print(f'{len(sum_substring) -1} различных подстрок в строке {string}')
+print('bc'.encode('utf-8').__hash__())
